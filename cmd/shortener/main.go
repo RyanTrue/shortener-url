d1fd642867f499c146c05f4bec5aa343e98d283a
Ryan D <zufirka@icloud.com>
2023-07-14 20:25:08 +0300

    merge

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"github.com/RyanTrue/shortener-url.git/internal/common/config"
-	"github.com/RyanTrue/shortener-url.git/internal/common/handler"
-	"github.com/RyanTrue/shortener-url.git/internal/common/server"
-	"github.com/RyanTrue/shortener-url.git/internal/common/service"
-
-	_ "github.com/lib/pq"
+	"github.com/RyanTrue/shortener-url.git/internal/app/config"
+	"github.com/RyanTrue/shortener-url.git/internal/app/handler"
+	"github.com/RyanTrue/shortener-url.git/internal/app/server"
+	"github.com/RyanTrue/shortener-url.git/internal/app/service"
 	"go.uber.org/zap"
 )
 
